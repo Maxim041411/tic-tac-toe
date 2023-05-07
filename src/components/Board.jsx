e613ef8a5e426010cc8fab0efe2f7c140b5b7ac5
Maxim Lomatchenko <alternative041@mail.ru>
2023-05-07 21:48:04 +0300

    tic-tac-toe

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Board.css'
 import Square from './Square';
+import s from './Square.css'
+
 
 const Board = ({squares, click}) => {
     return (
-        <div className='board'>
+        <div className= 'board'>
     {squares.map((i, index) => (
         <Square
             value={i}
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -10,39 +10,55 @@ const Game = () => {
 
     const handleClick = (index) => {
         const boardCopy = [...board]
-        if (winner || boardCopy [index]) return
-        boardCopy[index] = xIsNext ? 'X' : '0'
+        if (winner || boardCopy [index]) return null;
+        boardCopy[index] = xIsNext ? 'X' : 'O'
         setBoard(boardCopy)
         setXIsNext(!xIsNext)
+   
     }
+
+    function calculateWinner(squares) {
+        const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+          const [a, b, c] = lines[i];
+          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+          }
+        }
+        return null;
+      }
+
+      const startNewGame = () => {
+        return (
+        <button className='start_btn' onClick={() => setBoard(Array(9).fill(null))}> Очистить поле </button>
+          
+        )   
+        
+      }
 
 
     return (
         <div className='wrapper'>
-           <Board squares={board} click={handleClick} /> 
+        <p className='winner'>
+        {winner ? 'Победитель' + winner : 'Сейчас ходит:  ' + (xIsNext ? 'X' : 'O')}
+        </p>
+        
+           <Board squares={board} click={handleClick} />
+           {startNewGame()}
         </div>
     );
 }
 
-function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
+
   
 
 export default Game;
